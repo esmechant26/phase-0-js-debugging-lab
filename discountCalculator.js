96c129a80848f4c54a2edeb0f52e41469aa99dd1
Esme Chant <esmemchant@gmail.com>
2026-01-14 10:42:49 -0500

    debugging lab

--- discountCalculator.js
+++ discountCalculator.js
@@ -1,10 +1,11 @@
 function calculateDiscountedPrice(quantity, pricePerItem) {
+    debugger;
     let totalPrice = 0;
-    debugger
-    for (let i = 1; i < quantity; i++) {
+    debugger;
+    for (let i = 0; i < quantity; i++) {
         totalPrice += pricePerItem;
     }
-
+debugger;
     if (quantity >= 10) {
         totalPrice *= 0.9;
     }
@@ -12,6 +13,9 @@ function calculateDiscountedPrice(quantity, pricePerItem) {
     return totalPrice;
 }
 
+
+
+//console.log(calculateDiscountedPrice(5, 10))
 
 
 module.exports = calculateDiscountedPrice;
